b067070719fb80191c7e792a1eea1a68d70d7822
Kalo7o <kalo7o93@gmail.com>
2021-07-20 22:36:27 +0300

    Building a walking skeleton Part Two - Angular

--- API/Startup.cs
+++ API/Startup.cs
@@ -32,11 +32,12 @@ namespace API
             {
                 options.UseSqlite(this.config.GetConnectionString("DefaultConnection"));
             });
-            services.AddControllers();
             services.AddSwaggerGen(c =>
             {
                 c.SwaggerDoc("v1", new OpenApiInfo { Title = "API", Version = "v1" });
             });
+            services.AddControllers();
+            services.AddCors();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -52,6 +53,8 @@ namespace API
             app.UseHttpsRedirection();
 
             app.UseRouting();
+
+            app.UseCors(x => x.AllowAnyMethod().WithOrigins("https://localhost:4200"));
 
             app.UseAuthorization();
 
